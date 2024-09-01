90d80dd9dbcb6579ba0ddd4b533705bb17ba6e2d
Will <asampongg377@gmail.com>
2024-09-01 23:55:47 +0000

    Init

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const emailInput = document.querySelector('input');
 const button = document.querySelector('button');
 const form = document.querySelector('form');
-const mainContainer = document.querySelector('main-container');
-const success = document.querySelector('success');
+const mainContainer = document.querySelector('.main-container');
+const success = document.querySelector('.success');
+const error = document.querySelector('.error');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const email = emailInput.value;
+    const email = emailInput.value.trim();
     const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     console.log(email);
-})
+
+    if(validEmail.test(email)){
+        success.style.display = 'block';
+        mainContainer.style.display = 'none';
+        console.log('success');
+    } else {
+        success.style.display = 'none';
+        error.innerHTML = 'valid email required';
+        console.log('error');
+    }
+});
 
 emailInput.addEventListener('click', () => {
-    input.classList.toggle('active');
-    console.log('input');
+    emailInput.classList.toggle('active');
 });
 
 button.addEventListener('click', () => {
     button.classList.add('active');
-    console.log('button');
 });
 
